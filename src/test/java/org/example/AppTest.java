e00642bf87ace3e11dc95a11dcf8cbd79020c6f7
Daniel Gulei <daniel.ggradinaru@gmail.com>
2024-03-19 14:55:10 +0200

    lab2 tests

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAddStudentValid()
+    {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        int result = service.saveStudent("11", "aaa", 111);
+        assertEquals(result, 0);
+
+        Student added = new Student("11", "aaa", 111);
+        List<Student> list = new ArrayList<Student>();
+        Iterable<Student> students = service.findAllStudents();
+        students.forEach(list::add);
+        assertTrue(list.contains(added));
+    }
+
+    public void testAddStudentInvalid()
+    {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        int result = service.saveStudent("11", "aaa", 2);
+        assertEquals(result, 1);
+    }
+}
